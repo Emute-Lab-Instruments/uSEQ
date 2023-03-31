dcb49c10584cc6a241118d2e6ed1e652fc62ddd8
chris <chrisk13fer@gmail.com>
2023-03-31 15:59:52 +0100

    analog and digital write based on pinmap

---
+++ pinmap.h
@@ -0,0 +1,39 @@
+#ifndef PINMAP_H
+#define PINMAP_H
+
+#define LED_BOARD 25
+
+#define USEQ_PIN_D1 19
+#define USEQ_PIN_D2 18
+#define USEQ_PIN_D3 17
+#define USEQ_PIN_D4 16
+
+#define USEQ_PIN_A1 21
+#define USEQ_PIN_A2 20
+
+#define USEQ_PIN_I1 8
+#define USEQ_PIN_I2 9
+
+#define USEQ_PIN_LED_I1 5
+#define USEQ_PIN_LED_I2 4
+
+#define USEQ_PIN_LED_A1 3
+#define USEQ_PIN_LED_A2 2
+
+#define USEQ_PIN_LED_D1 28
+#define USEQ_PIN_LED_D2 27
+#define USEQ_PIN_LED_D3 26
+#define USEQ_PIN_LED_D4 22
+
+#define USEQ_PIN_SWITCH_M1 10
+#define USEQ_PIN_SWITCH_M2 11
+
+#define USEQ_PIN_SWITCH_T1 14
+#define USEQ_PIN_SWITCH_T2 15
+
+#define USEQ_PIN_SWITCH_R1 7
+
+#define USEQ_PIN_ROTARYENC_A 13
+#define USEQ_PIN_ROTARYENC_B 12
+
+#endif
