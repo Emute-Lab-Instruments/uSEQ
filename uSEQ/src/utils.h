5c939347dd70caf221731cceae02409c61eac66e
w1n5t0n <lm-w1n5t0n@protonmail.com>
2024-05-06 20:24:35 +0300

    Rename debug logger, add mutes and solos (TODO test)

--- uSEQ/src/utils.h
+++ uSEQ/src/utils.h
@@ -3,18 +3,32 @@
 
 #if USEQ_DEBUG
 // instantiates a local debugger object
-#define DBG(__name__) DEBUGGER local_debugger(__name__)
+#define DBG(__name__) DebugLogger local_debugger(__name__)
 // can be used within that scope to add to debug log
-#define dbg(__contents__) local_debugger.log(__contents__)
+#define dbg(__contents__) local_debugger.filtered_log(__contents__)
 
 /* EXAMPLE USAGE
+
+// (optional) specify mutes or solos
+
+DebugLogger::solos.insert("foo");
+// or
+DebugLogger::mutes.insert("bar");
+
+// Add local debuggers to functions/methods
 int foo(int x)
 {
     DBG("foo"); // First, constructor will log
     int result = x + 1;
-    dbg(result); // then, this will log
+    dbg("result: " + String(result)); // then, this will log
     return result; // finally, destructor will log
 }
+
+int bar()
+{
+    DBG("bar"); // this won't print
+}
+
 */
 
 #else
@@ -38,13 +52,6 @@ double millis();
 double micros();
 
 void delay(int);
-
-/* class PIO */
-/* { */
-/* }; */
-
-/* void pio_pwm_set_level(PIO, uint, uint32_t); */
-/* void pio_pwm_set_period(PIO, uint, uint32_t); */
 
 #endif // defined(ARDUINO)
 
