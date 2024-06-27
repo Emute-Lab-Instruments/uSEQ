2f0b96ae31d21dbd5b387007e3037ca200cfa844
w1n5t0n <lm-w1n5t0n@protonmail.com>
2024-06-27 11:14:36 +0100

    Fix println to accept any type, not just string

--- uSEQ/src/lisp/generated_builtins.cpp
+++ uSEQ/src/lisp/generated_builtins.cpp
@@ -1052,14 +1052,6 @@ Value println(std::vector<Value>& args, Environment& env)
         }
     }
 
-    // Checking individual args
-    if (!(args[0].is_string()))
-    {
-        report_error_wrong_specific_pred(user_facing_name, 1, "a string",
-                                         args[0].to_lisp_src());
-        return Value::error();
-    }
-
     // BODY
     Value result = Value::nil();
     String s;
