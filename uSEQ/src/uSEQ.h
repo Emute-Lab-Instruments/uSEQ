855fc4c0d36f91a8b7e2f136f2f95fc28f0e7f08
chriskiefer <chrisk13fer@gmail.com>
2024-11-05 22:48:23 +0000

    range low <= x <= high

--- uSEQ/src/uSEQ.h
+++ uSEQ/src/uSEQ.h
@@ -21,7 +21,7 @@
 #define LISP_FUNC_RETURN_TYPE Value
 #define LISP_FUNC_TYPE LISP_FUNC_RETURN_TYPE(LISP_FUNC_ARGS_TYPE)
 // For declaring builtin functions as class members
-#define LISP_FUNC_DECL(__name__) LISP_FUNC_RETURN_TYPE __name__(LISP_FUNC_ARGS)
+#define LISP_FUNC_DECL(__name__)  LISP_FUNC_RETURN_TYPE __name__(LISP_FUNC_ARGS)
 
 using TimeValue  = double;
 using PhaseValue = double;
