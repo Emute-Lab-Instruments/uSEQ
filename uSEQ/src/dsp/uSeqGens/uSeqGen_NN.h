507fe31b6d73e0d7817f29251bcc1905e92c7213
chriskiefer <chrisk13fer@gmail.com>
2025-05-13 23:46:19 +0100

    ugen-nn

--- uSEQ/src/dsp/uSeqGens/uSeqGen_NN.h
+++ uSEQ/src/dsp/uSeqGens/uSeqGen_NN.h
@@ -2,14 +2,40 @@
 #define USEQGEN_NN_H
 
 #include "uSeqGen_Base.h"
+#include "memlp/MLP.h"
 
 class uSeqGen_NN final : public uSeqGen_Base
 {
 public:
-uSeqGen_NN(queue_t *q, size_t key) : uSeqGen_Base(q, key)
+    uSeqGen_NN(queue_t *q, size_t key) : uSeqGen_Base(q, key)
     {
-        SetInputCount_(5);
-        SetOutputCount_(1);
+        const unsigned int kBias = 1;
+        const std::vector<ACTIVATION_FUNCTIONS> layers_activfuncs = {
+            RELU, RELU, RELU, SIGMOID
+        };
+        const bool use_constant_weight_init = false;
+        const float constant_weight_init = 0;
+        size_t n_inputs = 2;
+        size_t n_outputs = 1;
+        // Layer size definitions
+        const std::vector<size_t> layers_nodes = {
+            n_inputs + kBias,
+            10, 10, 14,
+            n_outputs
+        };
+
+        // Create MLP
+        mlp = std::make_unique<MLP<float>>(
+            layers_nodes,
+            layers_activfuncs,
+            loss::LOSS_MSE,
+            use_constant_weight_init,
+            constant_weight_init
+        );
+
+
+        SetInputCount_(n_inputs);
+        SetOutputCount_(n_outputs);
 
         // addMessageHandler("n", [this](float value) {
         //     n = static_cast<size_t>(value);
@@ -23,6 +49,9 @@ protected:
     {
         outputs.SetValue(0, 0.49f);
     }
+
+    std::unique_ptr<MLP<float>> mlp;
+
 
 private:
 };
