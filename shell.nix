c467e907296311ab7f2ac874704747865c1f3fde
w1n5t0n <w1n5t0n@lnfinitemonkeys.org>
2025-07-17 13:31:56 +0100

    Add shell.nix and build_arduino_cli.sh

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    arduino-cli
+    systemd  # provides libudev
+    libusb1
+  ];
+  
+  shellHook = ''
+    # Add nix lib paths to LD_LIBRARY_PATH for picotool
+    export LD_LIBRARY_PATH="${pkgs.systemd}/lib:${pkgs.libusb1}/lib:$LD_LIBRARY_PATH"
+    
+    echo "Arduino development environment loaded"
+    echo "Use: arduino-cli compile --fqbn rp2040:rp2040:generic uSEQ/"
+  '';
+}
