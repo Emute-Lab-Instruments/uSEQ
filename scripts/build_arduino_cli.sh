35c0d57496a95c8db96329264627c82a05ed0de1
w1n5t0n <w1n5t0n@lnfinitemonkeys.org>
2025-07-17 15:21:45 +0100

    Fix shell.nix for arduino-cli compile to work

--- scripts/build_arduino_cli.sh
+++ scripts/build_arduino_cli.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 # Build uSEQ firmware using arduino-cli
-arduino-cli compile --fqbn rp2040:rp2040:generic uSEQ/
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
+echo "Project dir: $PROJECT_DIR"
+arduino-cli compile --fqbn rp2040:rp2040:generic "$PROJECT_DIR/uSEQ/"
